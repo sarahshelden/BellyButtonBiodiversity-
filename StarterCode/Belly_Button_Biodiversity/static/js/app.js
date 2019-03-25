b2ee3de3429b89183a140110fa79b7e72d3bdecd
Sarah Shelden <sarah.shelden113@gmail.com>
2019-03-25 11:43:21 -0400

    javascipt / d3 started

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -4,7 +4,10 @@ function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
 
-    const json = d3.json(url ,callback);
+   var url = `/metadata/${sample}`;
+   d3.json(url).then(function (sample) {
+
+
 
     // Use d3 to select the panel with id of `#sample-metadata`
 
@@ -12,22 +15,29 @@ function buildMetadata(sample) {
 
     // Use `.html("") to clear any existing metadata
 
+        panel.html("");
 
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-}
+       Object.entries(sample).forEach(function ([key, value]) {
+           var row = panel.append("panel-body");
+           row.text(`{key}: ${value} \n`);
+
+        });
+});
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
 
-    d3.
+  var url = `/sample/${sample}`;
+  d3.json(url).then(function(sample) {
 
+  })
+}
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
